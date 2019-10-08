f47693813b393321803a7177bb8287f6951379da
Stephen Hillier <s.hillier@f5.com>
2019-10-08 15:03:15 -0400

    complete re-write

--- workspace/extensions/zlib/index.js
+++ workspace/extensions/zlib/index.js
@@ -7,23 +7,55 @@ var zlib = require('zlib');
 // Create a new rpc server for listening to TCL iRule calls.
 var ilx = new f5.ILXServer();
 
-ilx.addMethod('b64decode_and_inflate', function(req, res) {
+//function to decode encoded URLs
+function isEncoded (url) {
+    url = url || '';
+    return url !== decodeURIComponent(url);
+  };
+
+//function to decode URI components
+function cleanURI (uri) {
+    while (isEncoded(uri)) {
+      uri = decodeURIComponent(uri);
+    }
+    return uri;
+  };
+
+//function to inflate compressed content
+function inflateClean (samlResponse) {
+    if (samlResponse.includes('samlp:Response')) {
+      return samlResponse.toString('utf8');
+    } else {
+      var tmp = zlib.inflateRawSync(samlResponse).toString('utf8');
+      return tmp;
+    }
+  };
+
+//de-URL-encode, base64decode, inflate RAW SAML AuthN Request
+ilx.addMethod('decodeRAW', function(req, res) {
     try {
-        //try to unzip the payload
-        var inflate = zlib.inflate(new Buffer(req.params()[0], 'base64')).toString();
+        var rawData = req.params()[0];
+        var URLDecodedAssertion = cleanURI(rawData);
+        var B64Assertion = Buffer.from(URLDecodedAssertion, 'base64');
+        var rawAssertion = inflateClean(B64Assertion);
+        var xml = rawAssertion.toString('utf8').trim();
+        //console.log(xml);
     } catch (err) {
         //gracefully handle our errors ane return 0 (false==fail) because I was told do to that...
         console.error('Error: ', err.message);
         return res.reply(0);
     }
     //return 1 (true==success) and unzip'd content
-    res.reply([1, inflate]);
+    res.reply([1, xml]);
 });
 
-ilx.addMethod('b64decode_and_unzip', function(req, res) {
+//base64decode, inflate RAW SAML AuthN Request
+ilx.addMethod('b64decodeInflate', function(req, res) {
     try {
-        //try to unzip the payload
-        var inflate = zlib.unzip(new Buffer(req.params()[0], 'base64')).toString();
+        var URLDecodedAssertion = req.params()[0];
+        var B64Assertion = Buffer.from(URLDecodedAssertion, 'base64');
+        var rawAssertion = inflateClean(B64Assertion);
+        var xml = rawAssertion.toString('utf8').trim();
     } catch (err) {
         //gracefully handle our errors ane return resErr (0) because I was told do to that...
         console.error('Error: ', err.message);
